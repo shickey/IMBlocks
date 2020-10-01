53cfa734bfb31feb8cea83a9c7e20c0ea627e9e1
Sean Hickey <hisean@media.mit.edu>
2020-10-01 14:48:38 -0400

    Fixed wasm build script to include textures

--- examples/wasm/build.sh
+++ examples/wasm/build.sh
@@ -4,4 +4,4 @@ mkdir build
 emcc -g ../../Blocks/Blocks.cpp -o build/blocks.js -s EXPORTED_FUNCTIONS='["_InitBlocks", "_RunBlocks"]' -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "getValue", "setValue"]'
 cp index.html build/index.html
 cp imblocks.js build/imblocks.js
-
+cp -r textures build/textures
