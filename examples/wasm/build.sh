b296392ec168dcd2d3b0fc877c344ba19bea8bb9
Sean Hickey <hisean@media.mit.edu>
2020-10-01 14:45:29 -0400

    Wasm example

--- examples/wasm/build.sh
+++
@@ -1,7 +0,0 @@
-# Build blocks.wasm
-
-mkdir build
-emcc -g ../../Blocks/Blocks.cpp -o build/blocks.js -s EXPORTED_FUNCTIONS='["_InitBlocks", "_RunBlocks"]' -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "getValue", "setValue"]'
-cp index.html build/index.html
-cp imblocks.js build/imblocks.js
-
